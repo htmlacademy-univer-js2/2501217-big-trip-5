86f34ef6ed809cd43cd522eab8daacaaa68968e4
Илья Муравьев <ilyamuravyov@mail.ru>
2025-02-17 10:27:53 +0500

    Время разбивать камни

--- src/main.js
+++ src/main.js
@@ -2,6 +2,4 @@ import Presenter from './presenter/presenter.js';
 
 const filtersContainerElement = document.querySelector('.trip-controls__filters');
 const tripEventsContainerElement = document.querySelector('.trip-events');
-const presenter = new Presenter(filtersContainerElement, tripEventsContainerElement);
-
-presenter.init();
+new Presenter(filtersContainerElement, tripEventsContainerElement).init();
