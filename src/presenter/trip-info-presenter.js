4435124b7b006c6ce7c0b4cbbfadd56c9f0b21a2
Илья Муравьев <ilyamuravyov@mail.ru>
2025-05-19 11:52:21 +0500

    Доработки 3

--- src/presenter/trip-info-presenter.js
+++ src/presenter/trip-info-presenter.js
@@ -1,5 +1,6 @@
 import TripInfoView from '../view/trip-info-view.js';
-import { render, replace, RenderPosition } from '../framework/render.js';
+import { render, remove, replace, RenderPosition } from '../framework/render.js';
+import { getPointsDataRange, getTripPrice, getTripRoute } from '../utils/point.js';
 
 export default class TripInfoPresenter {
   #containerElement;
@@ -13,8 +14,22 @@ export default class TripInfoPresenter {
   }
 
   init() {
+    const points = this.#pointsListModel.points;
+
+    if (points.length === 0) {
+      if (this.#component) {
+        remove(this.#component);
+        this.#component = null;
+      }
+      return;
+    }
+
+    const dateRange = getPointsDataRange(points);
+    const routeCities = getTripRoute(points, this.#pointsListModel.destinations);
+    const totalPrice = getTripPrice(points, this.#pointsListModel.offers);
+
     const previousComponent = this.#component;
-    this.#component = new TripInfoView(this.#pointsListModel.points, this.#pointsListModel.destinations, this.#pointsListModel.offers);
+    this.#component = new TripInfoView(dateRange, routeCities, totalPrice);
 
     if (!previousComponent) {
       render(this.#component, this.#containerElement, RenderPosition.AFTERBEGIN);
